6be91d78fd9ddbab89a8c789992d98fd7261ccc3
CuteNeehh <105994116+Neehh@users.noreply.github.com>
2022-10-03 02:48:19 +0700

    Update __main__.py

--- neehproject/__main__.py
+++ neehproject/__main__.py
@@ -25,7 +25,7 @@ async def main():
             await bot.start()
             bot.me = await bot.get_me()
             await bot.join_chat("validc0de")
-            await bot.join_chat("hiroosupport")
+            await bot.join_chat("hiroxsupport")
             try:
                 await bot.send_message(
                     BOTLOG_CHATID, MSG_ON.format(BOT_VER, CMD_HANDLER)
@@ -43,7 +43,7 @@ async def main():
     if bot1 and str(BOTLOG_CHATID).startswith("-100"):
         bot1.me = await bot1.get_me()
         chat = await bot1.get_chat(BOTLOG_CHATID)
-        desc = "Group Log untuk Neeh-UserBot.\n\nHARAP JANGAN KELUAR DARI GROUP INI.\n\n✨ Powered By ~ @hiroosupport ✨"
+        desc = "Group Log untuk Neeh-UserBot.\n\nHARAP JANGAN KELUAR DARI GROUP INI.\n\n✨ Powered By ~ @hiroxsupport ✨"
         lolo = f"LOGS | FOR {bot1.me.first_name}"
         if chat.description != desc:
             await bot1.set_chat_description(BOTLOG_CHATID, desc)
